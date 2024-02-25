45da29f703733bba1abbae1275fc0772cb3554f1
Guilherme Serafin de Carvalho <98965845+guiserafin@users.noreply.github.com>
2024-02-25 21:27:16 +0000

    Adicionar arquivo de compose com serviço de banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
 function status(request, response) {
   response
     .status(200)
-    .json({ msg: "Alunos do curso.dev são pessoas acima da média" });
+    .json({ mensagem: "Alunos do curso.dev são pessoas acima da média" });
 }
 
 export default status;
